a76080c50648ca63f6517d404147362c7864b4a0
nam259377-afk <nam259377@gmail.com>
2026-09-21 00:22:56 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,25 @@
-const CACHE="namromah-22-v1";
+const CACHE="namromah-v3-1";
 const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
+self.addEventListener("install",event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  event.respondWith(
+    fetch(event.request).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});
+      return res;
+    }).catch(()=>caches.match(event.request))
+  );
+});
